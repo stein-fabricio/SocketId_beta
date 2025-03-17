8768237183ce894bca66f466acfe52adc5d17f2d
Fabricio Stein <fstein@tndev.net>
2025-03-17 09:42:45 -0300

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SocketId_beta",
+    platforms: [.iOS(.v16.6)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "SocketId_beta",
+            targets: ["SocketId_beta"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(name: "SocketId", path: "./Sources/SocketId.xcframework")
+    ]
+)
---
+++ Sources/SocketId.xcframework/ios-arm64/SocketId.framework/Headers/SocketId.h
@@ -0,0 +1,18 @@
+//
+//  SocketId.h
+//  SocketId
+//
+//  Created by Fabricio Stein on 17/03/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SocketId.
+FOUNDATION_EXPORT double SocketIdVersionNumber;
+
+//! Project version string for SocketId.
+FOUNDATION_EXPORT const unsigned char SocketIdVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SocketId/PublicHeader.h>
+
+
