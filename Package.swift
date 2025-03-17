d47eab57130d080c49739174cee289b08ac6c2e3
Fabricio Stein <fabricio.stein@goaugment.io>
2025-03-17 09:57:09 -0300

    main => SocketId target name

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SocketId_beta",
-            targets: ["SocketId_beta"]),
+            targets: ["SocketId"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
